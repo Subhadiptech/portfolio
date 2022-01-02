36353031fffbe91c985fd91ca1692c9668bd1676
Subhadiptech <2020ugee055@nitjsr.ac.in>
2022-01-02 16:43:34 +0530

    tailwind react setup done

---
+++ src/components/Head.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function header() {
+    return (
+        <div className='bg-pink-800'>
+            I am subhadip
+            
+
+        </div>
+    )
+}
