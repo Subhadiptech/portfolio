43d0d6b4dad0ad36e92edfb602a6ee3b6981f4c1
Subhadiptech <2020ugee055@nitjsr.ac.in>
2022-01-03 03:09:03 +0530

    NavBar Responsive Done

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import MenuList from './MenuList'
 
-export default function header() {
+export default function Head() {
+
+    const [toggle, setToggle] = useState(false)
+
+
+    function toggleMenu() {
+        setToggle(!toggle)
+    }
+
     return (
-        <nav className='bg-gray-800 text-white h-16 flex justify-between items-center z-10 sticky'>
-            <img className='w-20 p-2 h-16' src='logo.png' alt='logo'></img>
-            <ul className='flex'>
-                <li className='p-2'><a className='hover:text-yellow-400' href='#'>Home</a></li>
-                <li className='p-2'><a className='hover:text-yellow-400' href='#'>About</a></li>
-                <li className='p-2'><a className='hover:text-yellow-400' href='#'>Projects</a></li>
-                <li className='p-2'><a className='hover:text-yellow-400' href='#'>Blog</a></li>
-            </ul>
+        <div>
+            <nav className='bg-gray-800 text-white h-16 flex justify-between items-center z-50 sticky'>
+                <img className='w-20 p-2 h-16' src='logo.png' alt='logo'></img>
+                <ul className='sm:flex hidden'>
+                    <li className='p-2'><a className='hover:text-yellow-400' href='#'>Home</a></li>
+                    <li className='p-2'><a className='hover:text-yellow-400' href='#'>About</a></li>
+                    <li className='p-2'><a className='hover:text-yellow-400' href='#'>Projects</a></li>
+                    <li className='p-2'><a className='hover:text-yellow-400' href='#'>Blog</a></li>
+                    <button className='bg-yellow-400 hover:bg-transparent hover:border-2 hover:border-yellow-400 p-2 mr-4 rounded align-middle text-black hover:text-white'>Contact Me</button>
+                </ul>
+                <button className='sm:hidden' onClick={toggleMenu}><GiHamburgerMenu className='sm:hidden mx-4' /></button>
 
-            <button className='bg-yellow-400 hover:bg-transparent hover:border-2 hover:border-yellow-400 p-2 mr-4 rounded align-middle text-black hover:text-white'>Contact Me</button>
+            </nav>
+            {toggle ? <MenuList /> : null}
 
-        </nav>
+        </div>
     )
 }
