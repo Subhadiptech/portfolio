b55b47afa2e7353c7a05dc1d50b6609e51fc57c3
Nitin Rajesh <nitinrajesh094@gmail.com>
2022-02-15 14:29:44 +0530

    Added colors to icon

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,17 +13,12 @@ export default function Contact() {
             <main className='flex justify-evenly mt-6'>
 
            
-          <li className='hidden sm:block'><a href='mailto:subhadipdhn@gmail.com' target='_blank'><GrMail className='fill-white py-2 h-12 w-12 sm:h-16 sm:w-16 mt-4 hover:fill-red-600' /></a></li>
-        <li className='hidden sm:block'><a href='https://www.linkedin.com/in/subhadipdhn/' target='_blank'><GrLinkedin className='fill-white py-2 h-12 w-12 sm:h-16 sm:w-16 mt-4 hover:fill-blue-700' /></a></li>
-        <li className='hidden sm:block'><a href='https://twitter.com/Subhadipdhn' target='_blank'><GrTwitter className='fill-white py-2 h-12 w-12 sm:h-16 sm:w-16 mt-4 hover:fill-sky-500' /></a></li>
-        <li className='hidden sm:block'><a href='https://github.com/Subhadiptech' target='_blank'><GrGithub className='fill-white py-2 h-12 w-12 sm:h-16 sm:w-16 mt-4 hover:fill-slate-600' /></a></li>
+          <li><a href='mailto:subhadipdhn@gmail.com' target='_blank'><GrMail className='fill-white py-2 h-12 w-12 sm:h-16 sm:w-16 mt-4 fill-red-600 sm:fill-white hover:fill-red-600' /></a></li>
+        <li><a href='https://www.linkedin.com/in/subhadipdhn/' target='_blank'><GrLinkedin className='fill-white py-2 h-12 w-12 sm:h-16 sm:w-16 mt-4 fill-blue-700 sm:fill-white hover:fill-blue-700' /></a></li>
+        <li><a href='https://twitter.com/Subhadipdhn' target='_blank'><GrTwitter className='fill-white py-2 h-12 w-12 sm:h-16 sm:w-16 mt-4 fill-sky-500 sm:fill-white hover:fill-sky-500' /></a></li>
+        <li><a href='https://github.com/Subhadiptech' target='_blank'><GrGithub className='fill-white py-2 h-12 w-12 sm:h-16 sm:w-16 mt-4 fill-slate-600 sm:fill-white hover:fill-slate-600' /></a></li>
         
-          <li className='sm:hidden'><a href='mailto:subhadipdhn@gmail.com' target='_blank'><GrMail className='fill-white py-2 h-12 w-12 sm:h-16 sm:w-16 mt-4 fill-red-600' /></a></li>
-        <li className='sm:hidden'><a href='https://www.linkedin.com/in/subhadipdhn/' target='_blank'><GrLinkedin className='fill-white py-2 h-12 w-12 sm:h-16 sm:w-16 mt-4 fill-blue-700' /></a></li>
-        <li className='sm:hidden'><a href='https://twitter.com/Subhadipdhn' target='_blank'><GrTwitter className='fill-white py-2 h-12 w-12 sm:h-16 sm:w-16 mt-4 fill-sky-500' /></a></li>
-        <li className='sm:hidden'><a href='https://github.com/Subhadiptech' target='_blank'><GrGithub className='fill-white py-2 h-12 w-12 sm:h-16 sm:w-16 mt-4 fill-slate-600' /></a></li>
-      
-            
+         
             </main>
       
 
